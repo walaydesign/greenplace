1ba43d525e54d03d121fc2a0f7ef46278a24e304
youzhen <yl301114@gmail.com>
2025-02-13 01:31:12 +0000

    調整

--- public/js/common.js
+++ public/js/common.js
@@ -6,3 +6,14 @@ $(".nav_list_item.haschild").click(function() {
     $(this).parents("li").siblings("li").find(".nav_list_second").slideUp(300);
     $(this).parents("li").addClass("active").siblings("li").removeClass("active");
 })
+
+nav();
+function nav() {
+    $(".nav_list").children("li").each(function() {
+        if($(this).hasClass("active")) {
+            let liIndex = $(this).index();
+            let noborderIndex = liIndex - 1;
+            $(".nav_list").children("li").eq(noborderIndex).addClass("noborder");
+        }
+    })
+}
