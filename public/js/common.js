52d437be3db8e472395268d848d686cd0fa7af6a
youzhen <yl301114@gmail.com>
2025-02-13 00:08:17 +0000

    group

--- public/js/common.js
+++ public/js/common.js
@@ -1,2 +1,8 @@
 AOS.init({startEvent: 'load'});
 window.addEventListener('load', AOS.refresh);
+
+$(".nav_list_item.haschild").click(function() {
+    $(this).children(".nav_list_second").slideDown(300);
+    $(this).parents("li").siblings("li").find(".nav_list_second").slideUp(300);
+    $(this).parents("li").addClass("active").siblings("li").removeClass("active");
+})
